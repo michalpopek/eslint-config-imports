7b3ffa42e1b962d3c75b39f3dee8a01d93e85e91
Michał Popek <michal@popek.co>
2020-11-14 16:19:04 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  plugins: ["import"],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "never",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: false,
+        },
+      },
+    ],
+    "import/newline-after-import": "error",
+    "import/no-cycle": "error",
+  },
+};
